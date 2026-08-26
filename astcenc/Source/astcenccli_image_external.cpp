6f450dbc3512561c45c88a86e6922125d1559e45
helmesjo <helmesjo@gmail.com>
2026-08-26 16:28:23 +0200

    Re-patch astcenccli_image_external.cpp: Drop TINYEXR_USE_STB_ZLIB.

--- astcenc/Source/astcenccli_image_external.cpp
+++ astcenc/Source/astcenccli_image_external.cpp
@@ -35,13 +35,14 @@
 #define STBI_NO_PNM
 #define STBI_NO_PNG
 #define STBI_NO_PSD
-#define STBI_SUPPORT_ZLIB
 #define STBI_ASSERT(x) astcenc_runtime_assert(x)
 
 // Configure the TinyEXR library build.
+//
+// Do not set TINYEXR_USE_MINIZ or TINYEXR_USE_STB_ZLIB. libtinyexr is
+// packaged with miniz, and those macros are unsupported by the package.
+//
 #define TINYEXR_IMPLEMENTATION
-#define TINYEXR_USE_MINIZ 0
-#define TINYEXR_USE_STB_ZLIB 1
 #define TEXR_ASSERT(x) astcenc_runtime_assert(x)
 
 /**
